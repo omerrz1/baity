7fc18c0fabe658e2af17e6e689d5d6b158f3a3f0
Omer Majdi <omermajdi@Omers-MacBook-Air.local>
2023-01-30 19:24:59 +0000

    fixing emial bug

--- baity/settings.py
+++ baity/settings.py
@@ -135,8 +135,8 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='omer@baity.uk'
-EMAIL_HOST_PASSWORD = 'Baity1.py'
+EMAIL_HOST_USER ='omermajdi250@gmail.com'
+EMAIL_HOST_PASSWORD = 'mero999999'
 EMAIL_USE_TLS = True
 
 # Internationalization
