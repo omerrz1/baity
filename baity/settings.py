beb888ce0b5d7b9b636e530c08d323a94d2c4cb0
Omer Majdi <omermajdi@Omers-MacBook-Air.local>
2023-01-30 23:54:23 +0000

    security updates (v 1.0.0)

--- baity/settings.py
+++ baity/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0j3-0cj+tavjnmim3zgbl5g-##b0fj7p7#i5rpc$7a1wi1f-(1'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+SECRET_KEY = os.environ['secret_key']
 
-ALLOWED_HOSTS = ['omermajdi.pythonanywhere.com']
+
+DEBUG = os.environ['debug']
+
+ALLOWED_HOSTS = os.environ['allowed_hosts']
 CORS_ORIGIN_ALLOW_ALL = True
 
 MEDIA_URL = '/media/'
@@ -136,8 +136,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='baity@baity.uk'
-EMAIL_HOST_PASSWORD = '@BaityCantBeHacked1.py'
+EMAIL_HOST_USER = os.environ['main_email']
+EMAIL_HOST_PASSWORD = os.environ['host_password']
 
 
 # Internationalization
