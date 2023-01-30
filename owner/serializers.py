af5d3590084a011d6b587e53334c5641211c464b
Omer Majdi <omermajdi@Omers-MacBook-Air.local>
2023-01-30 21:36:16 +0000

    fixing emial bug

--- owner/serializers.py
+++ owner/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.conf import settings
+
 
 
 
@@ -20,13 +22,14 @@ class OwnerSerializer(serializers.ModelSerializer):
         user_email = validated_data['email']
         username = validated_data['username']
         message = f'hi {username} welcome to baity app' 
-        send_mail(
-            subject=f'welcome {username}',
-            message=message,
-            from_email='omermajdi250@gmail.com',
-            recipient_list=[user_email],
-            auth_password='mero999999'
+        email = EmailMessage(
+            f'wlecome {username}',
+            message,
+            settings.EMAIL_HOST_USER,
+            [user_email]
         )
+        email.fail_silently = False
+        email.send()
         return super().create(validated_data)
 
 
