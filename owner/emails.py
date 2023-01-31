2eef95ef6017e9da79594049776e13379f498201
Omer Majdi <omermajdi@Omers-MacBook-Air.local>
2023-01-31 23:12:47 +0000

    saving the otp to owner model

--- owner/emails.py
+++ owner/emails.py
@@ -14,9 +14,10 @@ def send_OTP(email, username):
     OTP = random.randint(1000,9999)
     owner = get_user_model()
     # getting the user object 
-    owner = owner.objects.all().filter(email = user_email)
+    owner = owner.objects.get(email=user_email)
     # storing their OTP
     owner.OTP = OTP
+    owner.save()
 
     message = render_to_string('OTP.html',{'username':username, 'OTP':OTP})
     subject = 'email verfication'
