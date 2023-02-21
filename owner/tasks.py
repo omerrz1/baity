bd183984ebcab4f09964440970b8a47535c62e5b
Omer Majdi <omermajdi@Omers-MacBook-Air.local>
2023-02-21 23:13:23 +0000

    testing my server

--- owner/tasks.py
+++ owner/tasks.py
@@ -1,4 +1,11 @@
-i = 0
-while i < 10:
-    print('server is working',i)
-    i+=i
+from django.contrib.auth import get_user_model
+import datetime
+
+def delete_inactive_users():
+    pass
+
+
+
+while True:
+    sometghing = input('enter something to test the server')
+    print(sometghing, 'was typed')
