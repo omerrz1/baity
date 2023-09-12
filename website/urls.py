512c3c3399fed563a590be1a7e40672dcfa16566
omerrz1 <113627375+omerrz1@users.noreply.github.com>
2023-09-12 23:28:12 +0100

    css

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns =[
-    path('', views.HomePage.as_view(),name='home-page')
+    path('', views.HomePage.as_view(),name='home-page'),
+    path('docs/', views.HomePage.as_view(),name='home-page')
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 # class based view for django template
 class HomePage(TemplateView):
     template_name = 'homepage.html'
+
+
+class docs(TemplateView):
+    template_name = 'doc.html'
