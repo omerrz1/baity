98f901a68100b86eb50059982f4ae9adbfd87e8c
omerrz1 <113627375+omerrz1@users.noreply.github.com>
2023-09-12 23:47:48 +0100

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns =[
     path('', views.HomePage.as_view(),name='home-page'),
-    path('docs/', views.HomePage.as_view(),name='home-page')
+    path('docs/', views.docs.as_view(),name='home-page')
 ]
